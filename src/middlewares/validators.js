cb1a3a8a38de67f2aa0a7be87366e088d5bb7095
shafanaura <shafanaura48@gmail.com>
2021-01-19 12:36:14 +0700

    add validation at movie

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -1,26 +1,13 @@
-const dbConn = require("../helpers/db");
-const { body, param, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 module.exports = {
-	userInfo: [
-		body("title", "Invalid email address")
+	movieInfo: [
+		body("title", "Please insert min 3 character")
 			.optional()
 			.isLength({ min: 3 })
 			.trim()
 			.unescape()
-			.escape()
-			.custom(async (value) => {
-				// Checking that the email already in use or NOT
-				const [
-					row,
-				] = await dbConn.execute(
-					"SELECT `title` FROM `movies` WHERE `title` =?",
-					[value],
-				);
-				if (row.length > 0) {
-					return Promise.reject("E-mail already in use");
-				}
-			}),
+			.escape(),
 	],
 	// Checking Validation Result
 	result: (req, res, next) => {
