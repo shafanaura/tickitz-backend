9ae880b72a480971e7c4d9ee7268861417ba4c21
shafanaura <shafanaura48@gmail.com>
2021-01-28 08:50:50 +0700

    add database

--- src/express_backend.sql
+++ src/express_backend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 27, 2021 at 07:58 AM
+-- Generation Time: Jan 27, 2021 at 05:12 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
@@ -33,7 +33,6 @@ CREATE TABLE `cinemas` (
   `picture` varchar(200) DEFAULT NULL,
   `address` varchar(100) NOT NULL,
   `price` int(11) NOT NULL,
-  `createdBy` int(11) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,10 +40,10 @@ CREATE TABLE `cinemas` (
 -- Dumping data for table `cinemas`
 --
 
-INSERT INTO `cinemas` (`id`, `name`, `picture`, `address`, `price`, `createdBy`, `createdAt`) VALUES
-(1, 'ebv.id', NULL, 'Whatever street No.12, South Purwokerto', 15, NULL, '2021-01-18 23:52:07'),
-(2, 'CineOne21', NULL, 'Downcare street No. 21, East Purwokerto', 12, NULL, '2021-01-18 23:52:36'),
-(3, 'hiflix Cinema', NULL, 'Colonel street No. 2, East Purwokerto', 10, NULL, '2021-01-18 23:53:07');
+INSERT INTO `cinemas` (`id`, `name`, `picture`, `address`, `price`, `createdAt`) VALUES
+(56, 'ebv.id', 'uploadspicture-1611756983120-318878915.png', 'Whatever street No.12, South Purwokerto', 12, '2021-01-27 14:16:23'),
+(57, 'CineOne21', 'uploadspicture-1611757055725-803277410.png', 'Downcare street No. 21, East Purwokerto', 15, '2021-01-27 14:17:35'),
+(62, 'hiflix Cinema', 'uploadspicture-1611757238426-602113244.png', 'Colonel street No. 2, East Purwokerto', 10, '2021-01-27 14:20:38');
 
 -- --------------------------------------------------------
 
@@ -64,9 +63,13 @@ CREATE TABLE `cinema_times` (
 --
 
 INSERT INTO `cinema_times` (`id`, `idCinema`, `idTime`, `createdAt`) VALUES
-(1, 1, 1, '2021-01-23 15:15:00'),
-(2, 1, 2, '2021-01-23 15:15:00'),
-(3, 1, 3, '2021-01-23 15:15:00');
+(30, 56, 1, '2021-01-27 14:16:23'),
+(31, 56, 2, '2021-01-27 14:16:23'),
+(32, 57, 2, '2021-01-27 14:17:35'),
+(33, 57, 4, '2021-01-27 14:17:35'),
+(42, 62, 4, '2021-01-27 14:20:38'),
+(43, 62, 5, '2021-01-27 14:20:38'),
+(44, 62, 6, '2021-01-27 14:20:38');
 
 -- --------------------------------------------------------
 
@@ -77,7 +80,6 @@ INSERT INTO `cinema_times` (`id`, `idCinema`, `idTime`, `createdAt`) VALUES
 CREATE TABLE `genres` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `createdBy` int(11) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -85,14 +87,14 @@ CREATE TABLE `genres` (
 -- Dumping data for table `genres`
 --
 
-INSERT INTO `genres` (`id`, `name`, `createdBy`, `createdAt`) VALUES
-(1, 'Action', NULL, '2021-01-20 08:07:32'),
-(2, 'Adventure', NULL, '2021-01-20 08:24:56'),
-(3, 'Comedy', NULL, '2021-01-20 08:25:54'),
-(4, 'Sci-Fi', NULL, '2021-01-20 08:26:32'),
-(5, 'War', NULL, '2021-01-20 08:26:39'),
-(6, 'Drama', NULL, '2021-01-20 08:26:45'),
-(12, 'komedi', NULL, '2021-01-21 03:55:28');
+INSERT INTO `genres` (`id`, `name`, `createdAt`) VALUES
+(1, 'Action', '2021-01-20 08:07:32'),
+(2, 'Adventure', '2021-01-20 08:24:56'),
+(3, 'Comedy', '2021-01-20 08:25:54'),
+(4, 'Sci-Fi', '2021-01-20 08:26:32'),
+(5, 'War', '2021-01-20 08:26:39'),
+(6, 'Drama', '2021-01-20 08:26:45'),
+(12, 'komedi', '2021-01-21 03:55:28');
 
 -- --------------------------------------------------------
 
@@ -144,7 +146,9 @@ INSERT INTO `movies` (`id`, `title`, `picture`, `releaseDate`, `directed`, `dura
 (137, 'Superman', 'uploadspicture-1611559795531-24019248.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', NULL, '2021-01-25 07:29:55'),
 (138, 'Minion', 'uploadspicture-1611559806303-201705803.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', NULL, '2021-01-25 07:30:06'),
 (139, 'Star Wars', 'uploadspicture-1611559819527-827521312.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', NULL, '2021-01-25 07:30:19'),
-(140, 'Iron Man', 'uploadspicture-1611559829765-447121450.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', NULL, '2021-01-25 07:30:29');
+(140, 'Iron Man', 'uploadspicture-1611559829765-447121450.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', NULL, '2021-01-25 07:30:29'),
+(141, 'Interstellar', 'uploadspicture-1611751968469-167045203.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', 9, '2021-01-27 12:52:48'),
+(142, 'Spider-Man: Homecomin', 'uploadspicture-1611751995908-630021288.png', '7 July 2017', 'Jon Watss', '2h 13min', 'Tom Holland, Michael Keaton, Robert Downey Jr.', 'Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.', 9, '2021-01-27 12:53:15');
 
 -- --------------------------------------------------------
 
@@ -164,9 +168,9 @@ CREATE TABLE `movie_genres` (
 --
 
 INSERT INTO `movie_genres` (`id`, `idMovie`, `idGenre`, `createdAt`) VALUES
-(90, 135, 1, '2021-01-25 07:29:10'),
+(90, 135, 2, '2021-01-25 07:29:10'),
 (91, 135, 2, '2021-01-25 07:29:10'),
-(92, 135, 4, '2021-01-25 07:29:10'),
+(92, 135, 2, '2021-01-25 07:29:10'),
 (93, 136, 1, '2021-01-25 07:29:36'),
 (94, 136, 5, '2021-01-25 07:29:36'),
 (95, 136, 6, '2021-01-25 07:29:36'),
@@ -174,14 +178,17 @@ INSERT INTO `movie_genres` (`id`, `idMovie`, `idGenre`, `createdAt`) VALUES
 (97, 137, 5, '2021-01-25 07:29:55'),
 (98, 137, 12, '2021-01-25 07:29:55'),
 (99, 138, 2, '2021-01-25 07:30:06'),
-(100, 138, 5, '2021-01-25 07:30:06'),
-(101, 138, 12, '2021-01-25 07:30:06'),
+(100, 138, 2, '2021-01-25 07:30:06'),
+(101, 138, 2, '2021-01-25 07:30:06'),
 (102, 139, 2, '2021-01-25 07:30:19'),
 (103, 139, 5, '2021-01-25 07:30:19'),
 (104, 139, 12, '2021-01-25 07:30:19'),
 (105, 140, 2, '2021-01-25 07:30:29'),
 (106, 140, 5, '2021-01-25 07:30:29'),
-(107, 140, 12, '2021-01-25 07:30:29');
+(107, 140, 12, '2021-01-25 07:30:29'),
+(108, 141, 1, '2021-01-27 12:52:48'),
+(109, 141, 2, '2021-01-27 12:52:48'),
+(110, 142, 4, '2021-01-27 12:53:15');
 
 -- --------------------------------------------------------
 
@@ -213,7 +220,6 @@ INSERT INTO `seats` (`id`, `name`, `createdAt`) VALUES
 CREATE TABLE `times` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `createdBy` int(11) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -221,14 +227,13 @@ CREATE TABLE `times` (
 -- Dumping data for table `times`
 --
 
-INSERT INTO `times` (`id`, `name`, `createdBy`, `createdAt`) VALUES
-(1, '08:30am', NULL, '2021-01-23 15:01:53'),
-(2, '10:30pm', NULL, '2021-01-23 15:02:05'),
-(3, '12:00pm', NULL, '2021-01-23 15:02:13'),
-(4, '02:00pm', NULL, '2021-01-23 15:02:19'),
-(5, '04:30pm', NULL, '2021-01-23 15:02:26'),
-(6, '07:00pm', NULL, '2021-01-23 15:02:34'),
-(7, '08:30pm', NULL, '2021-01-23 15:02:39');
+INSERT INTO `times` (`id`, `name`, `createdAt`) VALUES
+(1, '08:30am', '2021-01-23 15:01:53'),
+(2, '10:30pm', '2021-01-23 15:02:05'),
+(3, '12:00pm', '2021-01-23 15:02:13'),
+(4, '02:00pm', '2021-01-23 15:02:19'),
+(5, '04:30pm', '2021-01-23 15:02:26'),
+(6, '07:00pm', '2021-01-23 15:02:34');
 
 -- --------------------------------------------------------
 
@@ -252,7 +257,7 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `idUser`, `idMovie`, `idCinema`, `idTime`, `idLocation`, `dateTime`, `createdAt`) VALUES
-(2, 9, 137, 3, 2, 6, '0000-00-00', '2021-01-27 06:54:53');
+(26, 11, 136, 62, 4, 8, '2020-10-05', '2021-01-27 16:10:25');
 
 -- --------------------------------------------------------
 
@@ -272,8 +277,8 @@ CREATE TABLE `transaction_items` (
 --
 
 INSERT INTO `transaction_items` (`id`, `idTransaction`, `idSeat`, `createdAt`) VALUES
-(1, 2, 2, '2021-01-27 06:54:53'),
-(2, 2, 3, '2021-01-27 06:54:53');
+(49, 26, 2, '2021-01-27 16:10:25'),
+(50, 26, 3, '2021-01-27 16:10:25');
 
 -- --------------------------------------------------------
 
@@ -295,7 +300,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `email`, `password`, `role`, `createdAt`) VALUES
 (9, 'admin@gmail.com', '$2b$10$E0mQIsTTVqQyVyX1/LECped1IFVFKGISa5cJ3feV88olyuDSVZ27S', 'Admin', '2021-01-26 03:06:08'),
-(10, 'user1@gmail.com', '$2b$10$pXLN1W7Zzs5qTAuHt35pHegGQagiCKQ8VpUsE0avO85.u0oA1AlYa', 'SuperUser', '2021-01-26 03:10:47');
+(10, 'user1@gmail.com', '$2b$10$pXLN1W7Zzs5qTAuHt35pHegGQagiCKQ8VpUsE0avO85.u0oA1AlYa', 'SuperUser', '2021-01-26 03:10:47'),
+(11, 'user2@gmail.com', '$2b$10$KlknH76Ei3fZJa9EDbUF9.1ST3p8XHGVU/6I.aQmqzfrcnxbAnHua', 'SuperUser', '2021-01-27 16:08:36');
 
 --
 -- Indexes for dumped tables
@@ -305,8 +311,7 @@ INSERT INTO `users` (`id`, `email`, `password`, `role`, `createdAt`) VALUES
 -- Indexes for table `cinemas`
 --
 ALTER TABLE `cinemas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `createdBy` (`createdBy`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cinema_times`
@@ -320,8 +325,7 @@ ALTER TABLE `cinema_times`
 -- Indexes for table `genres`
 --
 ALTER TABLE `genres`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `createdBy` (`createdBy`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `locations`
@@ -354,8 +358,7 @@ ALTER TABLE `seats`
 -- Indexes for table `times`
 --
 ALTER TABLE `times`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `createdBy` (`createdBy`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `transactions`
@@ -390,77 +393,71 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cinemas`
 --
 ALTER TABLE `cinemas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `cinema_times`
 --
 ALTER TABLE `cinema_times`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
 
 --
 -- AUTO_INCREMENT for table `movie_genres`
 --
 ALTER TABLE `movie_genres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT for table `seats`
 --
 ALTER TABLE `seats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `times`
 --
 ALTER TABLE `times`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `transaction_items`
 --
 ALTER TABLE `transaction_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `cinemas`
---
-ALTER TABLE `cinemas`
-  ADD CONSTRAINT `cinemas_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `cinema_times`
@@ -468,12 +465,6 @@ ALTER TABLE `cinemas`
 ALTER TABLE `cinema_times`
   ADD CONSTRAINT `cinema_times_ibfk_2` FOREIGN KEY (`idTime`) REFERENCES `times` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `cinema_times_ibfk_3` FOREIGN KEY (`idCinema`) REFERENCES `cinemas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `genres`
---
-ALTER TABLE `genres`
-  ADD CONSTRAINT `genres_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `movies`
@@ -487,12 +478,6 @@ ALTER TABLE `movies`
 ALTER TABLE `movie_genres`
   ADD CONSTRAINT `movie_genres_ibfk_2` FOREIGN KEY (`idMovie`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `movie_genres_ibfk_3` FOREIGN KEY (`idGenre`) REFERENCES `genres` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
---
--- Constraints for table `times`
---
-ALTER TABLE `times`
-  ADD CONSTRAINT `times_ibfk_1` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transactions`
