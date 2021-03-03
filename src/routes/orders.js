4941f73164ac2d232797613a08447ef042e7ef41
shafanaura <shafanaura48@gmail.com>
2021-03-03 16:18:19 +0700

    initial

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -2,6 +2,6 @@ const routes = require("express").Router();
 const orderController = require("../controllers/order");
 const authMiddleware = require("../middlewares/auth");
 
-routes.post("/orders", orderController.createOrder);
+routes.post("/orders", authMiddleware.authCheck, orderController.createOrder);
 
 module.exports = routes;
