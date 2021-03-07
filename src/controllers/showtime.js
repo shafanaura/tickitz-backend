4654efa2877c19479b48db04dfdbdfc7c37dcc58
shafanaura <shafanaura48@gmail.com>
2021-03-07 19:06:44 +0700

    added times id

--- src/controllers/showtime.js
+++ src/controllers/showtime.js
@@ -6,162 +6,150 @@ const showtimeModel = require("../models/showtimes");
 const status = require("../helpers/Response");
 
 exports.createShowtime = async (req, res) => {
-	const data = req.body;
-	const selectedMovie = [];
-	const selectedLocation = [];
-	const selectedCinema = [];
-	const selectedTime = [];
-	// get location
-	if (typeof data.idMovie === "object") {
-		const results = await movieModel.checkMovie(data.idMovie);
-		if (results.length !== data.idMovie.length) {
-			return status.ResponseStatus(res, 400, "Some movie not exists");
-		} else {
-			results.forEach((item) => {
-				selectedMovie.push(item.id);
-			});
-		}
-	} else if (typeof data.idMovie === "string") {
-		const results = await movieModel.checkMovie([data.idMovie]);
-		if (results.length !== data.idMovie.length) {
-			return status.ResponseStatus(res, 400, "Some movie not exists");
-		} else {
-			results.forEach((item) => {
-				selectedMovie.push(item.id);
-			});
-		}
-	}
-	// get location
-	if (typeof data.idLocation === "object") {
-		const results = await locationModel.checkLocation(data.idLocation);
-		if (results.length !== data.idLocation.length) {
-			return status.ResponseStatus(res, 400, "Some location not exists");
-		} else {
-			results.forEach((item) => {
-				selectedLocation.push(item.id);
-			});
-		}
-	} else if (typeof data.idLocation === "string") {
-		const results = await locationModel.checkLocation([data.idLocation]);
-		if (results.length !== data.idLocation.length) {
-			return status.ResponseStatus(res, 400, "Some location not exists");
-		} else {
-			results.forEach((item) => {
-				selectedLocation.push(item.id);
-			});
-		}
-	}
-	// get cinema
-	if (typeof data.idCinema === "object") {
-		const results = await cinemaModel.checkCinema(data.idCinema);
-		if (results.length !== data.idCinema.length) {
-			return status.ResponseStatus(res, 400, "Some cinema not exists");
-		} else {
-			results.forEach((item) => {
-				selectedCinema.push(item.id);
-			});
-		}
-	} else if (typeof data.idCinema === "string") {
-		const results = await cinemaModel.checkCinema([data.idCinema]);
-		if (results.length !== data.idCinema.length) {
-			return status.ResponseStatus(res, 400, "Some cinema not exists");
-		} else {
-			results.forEach((item) => {
-				selectedCinema.push(item.id);
-			});
-		}
-	}
-	// get time
-	if (typeof data.idTime === "object") {
-		const results = await timeModel.checkTime(data.idTime);
-		if (results.length !== data.idTime.length) {
-			return status.ResponseStatus(res, 400, "Some time not exists");
-		} else {
-			results.forEach((item) => {
-				selectedTime.push(item.id);
-			});
-		}
-	} else if (typeof data.idTime === "string") {
-		const results = await timeModel.checkTime([data.idTime]);
-		if (results.length !== data.idTime.length) {
-			return status.ResponseStatus(res, 400, "Some time not exists");
-		} else {
-			results.forEach((item) => {
-				selectedTime.push(item.id);
-			});
-		}
-	}
-	const showtimeData = {
-		showTimeDate: data.showTimeDate,
-		idMovie: data.idMovie,
-		idLocation: data.idLocation,
-		idCinema: data.idCinema,
-		idTime: data.idTime,
-	};
-	if (showtimeData) {
-		console.log("Berhasil");
-	}
-	// const initialResult = await showtimeModel.createShowtime(showtimeData);
-	// if (initialResult.affectedRows > 0) {
-	// 	const resultsData = await showtimeModel.getShowtimeById(
-	// 		initialResult.insertId,
-	// 	);
-	// 	if (resultsData.length > 0) {
-	// 		return status.ResponseStatus(res, 200, "Order successfully created", {
-	// 			// id: resultsData[0].id,
-	// 			// user: resultsData[0].userName,
-	// 			// movie: resultsData[0].title,
-	// 			// cinema: resultsData[0].cinemaName,
-	// 			// time: resultsData[0].timeName,
-	// 			// location: resultsData[0].locationName,
-	// 			// date: resultsData[0].dateTime,
-	// 		});
-	// 	} else {
-	// 		return status.ResponseStatus(res, 400, "Failed to create order");
-	// 	}
-	// }
+  const data = req.body;
+  const selectedMovie = [];
+  const selectedLocation = [];
+  const selectedCinema = [];
+  const selectedTime = [];
+  // get location
+  if (typeof data.idMovie === "object") {
+    const results = await movieModel.checkMovie(data.idMovie);
+    if (results.length !== data.idMovie.length) {
+      return status.ResponseStatus(res, 400, "Some movie not exists");
+    } else {
+      results.forEach((item) => {
+        selectedMovie.push(item.id);
+      });
+    }
+  } else if (typeof data.idMovie === "string") {
+    const results = await movieModel.checkMovie([data.idMovie]);
+    if (results.length !== data.idMovie.length) {
+      return status.ResponseStatus(res, 400, "Some movie not exists");
+    } else {
+      results.forEach((item) => {
+        selectedMovie.push(item.id);
+      });
+    }
+  }
+  // get location
+  if (typeof data.idLocation === "object") {
+    const results = await locationModel.checkLocation(data.idLocation);
+    if (results.length !== data.idLocation.length) {
+      return status.ResponseStatus(res, 400, "Some location not exists");
+    } else {
+      results.forEach((item) => {
+        selectedLocation.push(item.id);
+      });
+    }
+  } else if (typeof data.idLocation === "string") {
+    const results = await locationModel.checkLocation([data.idLocation]);
+    if (results.length !== data.idLocation.length) {
+      return status.ResponseStatus(res, 400, "Some location not exists");
+    } else {
+      results.forEach((item) => {
+        selectedLocation.push(item.id);
+      });
+    }
+  }
+  // get cinema
+  if (typeof data.idCinema === "object") {
+    const results = await cinemaModel.checkCinema(data.idCinema);
+    if (results.length !== data.idCinema.length) {
+      return status.ResponseStatus(res, 400, "Some cinema not exists");
+    } else {
+      results.forEach((item) => {
+        selectedCinema.push(item.id);
+      });
+    }
+  } else if (typeof data.idCinema === "string") {
+    const results = await cinemaModel.checkCinema([data.idCinema]);
+    if (results.length !== data.idCinema.length) {
+      return status.ResponseStatus(res, 400, "Some cinema not exists");
+    } else {
+      results.forEach((item) => {
+        selectedCinema.push(item.id);
+      });
+    }
+  }
+  // get time
+  if (typeof data.idTime === "object") {
+    const results = await timeModel.checkTime(data.idTime);
+    if (results.length !== data.idTime.length) {
+      return status.ResponseStatus(res, 400, "Some time not exists");
+    } else {
+      results.forEach((item) => {
+        selectedTime.push(item.id);
+      });
+    }
+  } else if (typeof data.idTime === "string") {
+    const results = await timeModel.checkTime([data.idTime]);
+    if (results.length !== data.idTime.length) {
+      return status.ResponseStatus(res, 400, "Some time not exists");
+    } else {
+      results.forEach((item) => {
+        selectedTime.push(item.id);
+      });
+    }
+  }
+  const showtimeData = {
+    showTimeDate: data.showTimeDate,
+    idMovie: data.idMovie,
+    idLocation: data.idLocation,
+    idCinema: data.idCinema,
+    idTime: data.idTime,
+  };
+  if (showtimeData) {
+    console.log("Berhasil");
+  }
+  // const initialResult = await showtimeModel.createShowtime(showtimeData);
+  // if (initialResult.affectedRows > 0) {
+  // 	const resultsData = await showtimeModel.getShowtimeById(
+  // 		initialResult.insertId,
+  // 	);
+  // 	if (resultsData.length > 0) {
+  // 		return status.ResponseStatus(res, 200, "Order successfully created", {
+  // 			// id: resultsData[0].id,
+  // 			// user: resultsData[0].userName,
+  // 			// movie: resultsData[0].title,
+  // 			// cinema: resultsData[0].cinemaName,
+  // 			// time: resultsData[0].timeName,
+  // 			// location: resultsData[0].locationName,
+  // 			// date: resultsData[0].dateTime,
+  // 		});
+  // 	} else {
+  // 		return status.ResponseStatus(res, 400, "Failed to create order");
+  // 	}
+  // }
 };
 
 exports.listShowtime = async (req, res) => {
-	const cond = { ...req.query };
-	cond.date = cond.date || "";
-	cond.location = cond.location || "";
-	cond.movie = cond.movie || "";
+  const cond = { ...req.query };
+  cond.date = cond.date || "";
+  cond.location = cond.location || "";
+  cond.movie = cond.movie || "";
 
-	const resultGetTimes = await showtimeModel.getShowtimeByCondition(cond);
-	let output = {};
-	resultGetTimes.forEach((item) => {
-		// the item does not exists, we create it.
-		if (!output[item.cinemaName]) {
-			output[item.cinemaName] = {
-				id: item.cinemaId,
-				cinema: item.cinemaName,
-				picture: item.cinemaPicture,
-				address: item.cinemaAddress,
-				price: item.cinemaPrice,
-				times: [],
-			};
-		}
-		// in either case, we push the current item in the times.
-		// of the current output key.
-		output[item.cinemaName].times.push(item.timeName);
-	});
-
-	// we are using Object.values() because we do not want the keys
-	// used to generate the output.
-	const results = Object.values(output);
-
-	// const arr = resultGetTimes.reduce((r, item) => {
-	// 	r[item.cinemaName] = r[item.cinemaName] || [];
-	// 	r[item.cinemaName].push(item.timeName);
-	// 	return r;
-	// }, {});
-	// const results = Object.keys(arr).map((key) => ({
-	// 	cinemaName: key,
-	// 	timeName: arr[key],
-	// }));
-	console.log(results);
-	if (results) {
-		return status.ResponseStatus(res, 200, "List of all showtime", results);
-	}
+  const resultGetTimes = await showtimeModel.getShowtimeByCondition(cond);
+  const output = {};
+  resultGetTimes.forEach((item) => {
+    if (!output[item.cinemaName]) {
+      output[item.cinemaName] = {
+        id: item.cinemaId,
+        cinema: item.cinemaName,
+        picture: item.cinemaPicture,
+        address: item.cinemaAddress,
+        price: item.cinemaPrice,
+        times: [],
+      };
+    }
+    // output[item.cinemaName].times.push(item.timeName);
+    output[item.cinemaName].times.push({
+      time: item.timeName,
+      id: item.timeId,
+    });
+  });
+  const results = Object.values(output);
+  console.log(results);
+  if (results) {
+    return status.ResponseStatus(res, 200, "List of all showtime", results);
+  }
 };
--- src/models/showtimes.js
+++ src/models/showtimes.js
@@ -2,82 +2,84 @@ const dbConn = require("../helpers/db");
 const table = "showtimes";
 
 exports.createShowtime = async (data = {}) => {
-	return new Promise((resolve, reject) => {
-		dbConn.query(
-			`INSERT INTO ${table} (${Object.keys(
-				data,
-			).join()}) VALUES (${Object.values(data)
-				.map((item) => `"${item}"`)
-				.join(",")})`,
-			(err, res, field) => {
-				if (err) reject(err);
-				resolve(res);
-			},
-		);
-	});
+  return new Promise((resolve, reject) => {
+    dbConn.query(
+      `INSERT INTO ${table} (${Object.keys(
+        data
+      ).join()}) VALUES (${Object.values(data)
+        .map((item) => `"${item}"`)
+        .join(",")})`,
+      (err, res, field) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
+  });
 };
 
 exports.createBulkShowtime = async (id, data = []) => {
-	return new Promise((resolve, reject) => {
-		dbConn.query(
-			`INSERT INTO ${table} (idMovie, idSeat) VALUES ${data
-				.map((idSeat) => `(${id}, ${idSeat})`)
-				.join()}`,
-			(err, res, field) => {
-				if (err) reject(err);
-				resolve(res);
-			},
-		);
-	});
+  return new Promise((resolve, reject) => {
+    dbConn.query(
+      `INSERT INTO ${table} (idMovie, idSeat) VALUES ${data
+        .map((idSeat) => `(${id}, ${idSeat})`)
+        .join()}`,
+      (err, res, field) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
+  });
 };
 
 exports.getShowtimeByCondition = (cond) => {
-	return new Promise((resolve, reject) => {
-		dbConn.query(
-			`
-			SELECT cinemas.id as cinemaId, cinemas.name as cinemaName, cinemas.picture as cinemaPicture, 
+  return new Promise((resolve, reject) => {
+    dbConn.query(
+      `
+			SELECT cinemas.id as cinemaId, cinemas.name as cinemaName, 
+			cinemas.picture as cinemaPicture, 
 			cinemas.address as cinemaAddress, cinemas.price as cinemaPrice,
-			times.name as timeName, times.id as timeId FROM ${table}
-			INNER JOIN cinemas ON cinemas.id = ${table}.idCinema
-			INNER JOIN times ON times.id = ${table}.idTime
+			times.name as timeName, times.id as timeId 
+			FROM ${table} s
+			INNER JOIN cinemas ON cinemas.id = s.idCinema
+			INNER JOIN times ON times.id = s.idTime
 			WHERE showTimeDate LIKE "${cond.date}"
 			AND idLocation LIKE "${cond.location}"
 			AND idMovie LIKE "${cond.movie}"
 			`,
-			(err, res, field) => {
-				if (err) reject(err);
-				resolve(res);
-			},
-		);
-	});
+      (err, res, field) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
+  });
 };
 
 exports.getLocDate = (id) => {
-	return new Promise((resolve, reject) => {
-		dbConn.query(
-			`
+  return new Promise((resolve, reject) => {
+    dbConn.query(
+      `
 			SELECT showTimeDate, idLocation FROM ${table} WHERE idMovie = ${id}
 			`,
-			(err, res, field) => {
-				if (err) reject(err);
-				resolve(res);
-			},
-		);
-	});
+      (err, res, field) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
+  });
 };
 
 exports.getShowOrder = (id) => {
-	return new Promise((resolve, reject) => {
-		dbConn.query(
-			`
+  return new Promise((resolve, reject) => {
+    dbConn.query(
+      `
 			SELECT * FROM ${table} WHERE id=${id}
 			`,
-			(err, res, field) => {
-				if (err) reject(err);
-				resolve(res);
-			},
-		);
-	});
+      (err, res, field) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
+  });
 };
 
 // exports.getShowOrder = (cond) => {
